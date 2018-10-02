06b9cc72afebd9f06fae1f8f3504c1342fe3f78b
Aibek Smagulov <smagulovaybek@gmail.com>
2018-10-02 14:13:17 +0900

    Init of the project
    The udactiy full-stack catalog project has been initialized with .gitignore and some other files.

---
+++ vagrant/catalog/catalog.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, session, redirect
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+@app.route("/cities")
+@app.route("/")
+def hello():
+    return "Hello world!"
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
