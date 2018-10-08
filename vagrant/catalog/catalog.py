2d21ebd465c60999da39e17a59e6b39ccc3dbb82
Aibek Smagulov <smagulovaybek@gmail.com>
2018-10-08 11:04:24 +0900

    authentication added with help of google oauth api

--- vagrant/catalog/catalog.py
+++ vagrant/catalog/catalog.py
@@ -2,6 +2,15 @@ from flask import Flask, request, session, redirect, flash, url_for, render_temp
 from models.index import City, Base, Restaurant, User, Complaint, Recommendation
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import asc, create_engine
+from flask import session as login_session
+import random
+import string
+from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import FlowExchangeError
+import httplib2
+import json
+from flask import make_response
+import requests
 
 app = Flask(__name__)
 app.secret_key = "SECRET_KEY"
@@ -9,6 +18,103 @@ engine = create_engine("postgresql:///catalogdb")
 Base.metadata.bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
+
+CLIENT_ID = json.loads(
+    open('client_secrets.json', 'r').read())['web']['client_id']
+APPLICATION_NAME = "Restaurant Menu Application"
+
+
+@app.route("/")
+def showSignIn():
+    return render_template("signin.html")
+
+
+@app.route("/signin/user")
+def showSignInUser():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    login_session['admin'] = False
+    return render_template("login.html", STATE=state)
+
+
+@app.route("/signin/admin")
+def showSignInAdmin():
+    state = ''.join(random.choice(string.ascii_uppercase + string.digits)
+                    for x in xrange(32))
+    login_session['state'] = state
+    login_session['admin'] = True
+    return render_template("login.html", STATE=state)
+
+
+def getUserData(credentials):
+    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
+    params = {'access_token': credentials.access_token, 'alt': 'json'}
+    answer = requests.get(userinfo_url, params=params)
+    return answer.json()
+
+
+def saveUserToDB(data):
+    login_session['username'] = data['name'] if 'name' in data else ''
+    login_session['email'] = data['email']
+    if session.query(User).filter_by(email=data['email']).first() is None:
+        user = User(
+            name=login_session['username'],
+            email=data['email'],
+            is_admin=login_session['admin'])
+        session.add(user)
+        flash('New user `%s` was created' % user.name)
+
+
+@app.route('/gconnect/', methods=['POST'])
+def gconnect():
+    if request.args.get('state') != login_session['state']:
+        response = make_response(json.dumps('Invalid state parameter.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    code = request.data
+    try:
+        oauth_flow = flow_from_clientsecrets('client_secrets.json', scope='')
+        oauth_flow.redirect_uri = 'postmessage'
+        credentials = oauth_flow.step2_exchange(code)
+    except FlowExchangeError:
+        response = make_response(
+            json.dumps('Failed to upgrade the authorization code.'), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    access_token = credentials.access_token
+    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
+           % access_token)
+    h = httplib2.Http()
+    result = json.loads(h.request(url, 'GET')[1])
+    if result.get('error') is not None:
+        response = make_response(json.dumps(result.get('error')), 500)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    gplus_id = credentials.id_token['sub']
+    if result['user_id'] != gplus_id:
+        response = make_response(
+            json.dumps("Token's user ID doesn't match given user ID."), 401)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    if result['issued_to'] != CLIENT_ID:
+        response = make_response(
+            json.dumps("Token's client ID does not match app's."), 401)
+        print "Token's client ID does not match app's."
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    stored_access_token = login_session.get('access_token')
+    stored_gplus_id = login_session.get('gplus_id')
+    data = getUserData(credentials)
+    saveUserToDB(data)
+    if stored_access_token is not None and gplus_id == stored_gplus_id:
+        response = make_response(json.dumps('Current user is already connected.'),
+                                 200)
+        response.headers['Content-Type'] = 'application/json'
+        return response
+    login_session['access_token'] = credentials.access_token
+    login_session['gplus_id'] = gplus_id
+    return 'True'
 
 
 @app.route("/city/new/", methods=["GET", "POST"])
